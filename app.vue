7a97e2d5e435506d8ace92d4d29b195e87dcfdd4
atsumim <atsumim@ikyu.com>
2023-05-30 18:41:06 +0900

    :heavy_plus_sign: add eslint

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <span>Hello</span>
   </div>
 </template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      msg: 'Welcome to Nuxt3',
+    };
+  },
+});
+</script>
+
